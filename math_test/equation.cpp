3500823e4133de4f26b89b82bf32ff8c01313d67
yatsNikitayats <yatsishin.nik@mail.ru>
2026-04-17 14:03:46 +0300

    commit
    Added comment in solve method to clarify class methods.

--- math_test/equation.cpp
+++ math_test/equation.cpp
@@ -1,7 +1,7 @@
 #include "equation.h"
 
 int Equation::solve(double a, double b, double c, double& x1, double& x2) {
-    if (fabs(a) < EPS) return -1;//
+    if (fabs(a) < EPS) return -1;////все методы внутри класса
 
     double D = b * b - 4 * a * c;
 
