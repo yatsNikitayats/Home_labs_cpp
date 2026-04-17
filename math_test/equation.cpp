8dc301386201fe203d1545c59cf7963abec26104
yatsNikitayats <yatsishin.nik@mail.ru>
2026-04-17 14:03:06 +0300

    commit

--- math_test/equation.cpp
+++ math_test/equation.cpp
@@ -1,7 +1,7 @@
 #include "equation.h"
 
 int Equation::solve(double a, double b, double c, double& x1, double& x2) {
-    if (fabs(a) < EPS) return -1;////все методы внутри класса
+    if (fabs(a) < EPS) return -1;//
 
     double D = b * b - 4 * a * c;
 
